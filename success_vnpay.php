c96a26b0357b1bb90f3e7d848263559c4dfc317a
Trần Quang Huy <92586415+Conan286@users.noreply.github.com>
2024-05-13 17:18:21 +0700

    update success_vnpay.php

--- success_vnpay.php
+++ success_vnpay.php
@@ -4,7 +4,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET")
     if ($_GET['vnp_ResponseCode'] == '00') 
     {
 
-        header('Location: ' . 'http://localhost/shopthoitrang/success_result.php');
+        header('Location: ' . 'http://localhost/shopbongda/success_result.php');
     }
     else {
         echo ('Lỗi giao dịch');
